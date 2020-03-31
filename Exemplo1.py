d836e85a63effab68cc9628708edcc76b7855465
gui.matozinho <60327278+guimatozinho@users.noreply.github.com>
2020-03-31 08:52:32 -0300

    Add files via upload

---
+++ Exemplo1.py
@@ -0,0 +1,24 @@
+import gi
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+builder = Gtk.Builder()
+builder.add_from_file('user_interface.glade')
+
+class Manipulador(object):
+    def __init__(self):
+        self.entry = builder.get_object('entry')
+        self.label = builder.get_object('label')
+
+    def on_button_clicked(self, button):
+        text = self.entry.get_text()
+        self.label.set_text(text)
+
+    def on_main_window_destroy(self, window):
+        Gtk.main_quit()
+
+builder.connect_signals(Manipulador())
+window = builder.get_object('main_window')
+window.show_all()
+Gtk.main()
